1d0a856d483180ed8e390251eae3481a19598263
Feng Guo <feng.guo2001@gmail.com>
2019-02-13 19:10:10 -0500

    added private classes

--- TicketingSystem.java
+++ TicketingSystem.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.awt.Toolkit;
 
@@ -7,6 +9,8 @@ public class TicketingSystem extends JFrame{
     private int numberOfTables;
     private int peoplePerTable;
     private ArrayList<Student> masterListOfStudents;
+    private MainScreen mainScreen;
+    private StudentForm studentForm;
 
     public int getNumberOfTables() {
         return numberOfTables;
@@ -49,7 +53,86 @@ public class TicketingSystem extends JFrame{
      this.setVisible(true);
      this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
      this.setSize(Toolkit.getDefaultToolkit().getScreenSize());
-     
+     init();
+     setScreen("MainScreen");
 
+    }
+
+    private void setScreen(String screen) {
+        getContentPane().removeAll();
+        if (screen.equals("StudentForm")) {
+            getContentPane().add(studentForm);
+        } else if (screen.equals("MainScreen")) {
+            getContentPane().add(mainScreen);
+        } else if (screen.equals("StartScreen")) {
+            //build it
+        } else if (screen.equals("Search")) {
+            //build it
+        }
+    }
+
+    private void init() {
+        mainScreen = new MainScreen();
+        studentForm = new StudentForm();
+    }
+
+
+    private class MainScreen extends JPanel {
+        JButton addStudentButton;
+        JButton searchButton;
+        JButton arrangeTablesButton;
+        JButton saveButton;
+        JButton backButton;
+        JButton displayTablesButton;
+
+        MainScreen() {
+            super();
+            addStudentButton = new JButton("Add Student");
+            addStudentButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent actionEvent) {
+                    setScreen("StudentForm");
+                }
+            });
+            searchButton = new JButton("Search");
+            arrangeTablesButton = new JButton("Arrange Tables");
+            saveButton = new JButton("Save");
+            backButton = new JButton("Back");
+            displayTablesButton = new JButton("Display Tables");
+            add(addStudentButton);
+            add(searchButton);
+            add(arrangeTablesButton);
+            add(saveButton);
+            add(backButton);
+            add(displayTablesButton);
+        }
+    }
+
+    private class StudentForm extends JPanel {
+        JLabel firstNameLabel;
+        JLabel lastNameLabel;
+        JLabel studentNumberLabel;
+        JLabel allergiesLabel;
+        JLabel friendPreferencesLabel;
+        JTextField firstNameTextField;
+        JTextField lastNameTextField;
+        JTextField studentNumberTextField;
+        JTextField[] friendsTextField = new JTextField[9];
+
+        StudentForm() {
+            firstNameLabel = new JLabel("First Name");
+            lastNameLabel = new JLabel("Last Name");
+            studentNumberLabel = new JLabel("Student Number");
+            allergiesLabel = new JLabel("Allergies");
+            friendPreferencesLabel = new JLabel("Friend Student Numbers");
+            firstNameTextField = new JTextField();
+            lastNameTextField = new JTextField();
+            studentNumberTextField = new JTextField();
+            //JTextField[] friendsTextField = new JTextField[9];
+            add(firstNameLabel);
+            add(firstNameTextField);
+            add(lastNameLabel);
+            add(lastNameTextField);
+        }
     }
 }
